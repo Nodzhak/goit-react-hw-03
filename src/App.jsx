c89963aaacb3b9a4e8fd2694be5ae463392a5b7d
Nodzhak <kostiantyn.nodzhak@gmail.com>
2024-02-25 16:57:45 +0100

    The end
    The end

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import SearchBox from './components/SearchBox/SearchBox.jsx';
 import ContactList from './components/ContactList/ContactList.jsx';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { "id": "id-1", "name": "Rosie Simpson", "number": "459-12-56" },
-    { "id": "id-2", "name": "Hermione Kline", "number": "443-89-12" },
-    { "id": "id-3", "name": "Eden Clements", "number": "645-17-79" },
-    { "id": "id-4", "name": "Annie Copeland", "number": "227-91-26" }
-  ]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  );
 
   const [filterValue, setFilterValue] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleAddContact = newContact => {
     setContacts(prevContacts => [...prevContacts, newContact]);
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,26 +2,22 @@ import React from 'react';
 import Contact from './Contact.jsx';
 import styles from './ContactList.module.css';
 
-class ContactList extends React.Component {
-  render() {
-    const { contacts, onRemoveContact } = this.props;
-
-    return (
-      <>
-        <ul className={styles.contactList}>
-          {contacts.map(({ id, name, number }) => (
-            <Contact
-              key={id}
-              id={id}
-              name={name}
-              number={number}
-              onRemoveContact={onRemoveContact}
-            />
-          ))}
-        </ul>
-      </>
-    );
-  }
-}
+const ContactList = ({ contacts, onRemoveContact }) => {
+  return (
+    <>
+      <ul className={styles.contactList}>
+        {contacts.map(({ id, name, number }) => (
+          <Contact
+            key={id}
+            id={id}
+            name={name}
+            number={number}
+            onRemoveContact={onRemoveContact}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default ContactList;
